69c39407d0d0fa4048d6f245cc753191954306ce
Yogesh Lokare <yogesh.lokare@mjbtech.com>
2020-12-04 01:05:26 -0700

    updated db js file

--- src/models/db.js
+++ src/models/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
+/*const connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
@@ -15,4 +15,4 @@ connection.connect(error => {
   console.log("Successfully connected to the database.");
 });
 
-module.exports = connection
+module.exports = connection*/
